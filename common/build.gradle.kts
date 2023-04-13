f85b4226c59b69b21f4b4b518a207f2aa3afe5c8
Litrik De Roy <litrik@norio.be>
2023-04-13 17:00:53 +0200

    Update Junit [4.13 -> 4.13.2]

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -105,7 +105,7 @@ kotlin {
         }
         val androidTest by getting {
             dependencies {
-                implementation("junit:junit:4.13")
+                implementation("junit:junit:4.13.2")
             }
         }
         val desktopMain by getting {
